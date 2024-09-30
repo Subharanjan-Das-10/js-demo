b85afa266f72ab82b5124289b73c6bdac9a6c489
Subharanjan-Das-10 <subharanjandas10@gmail.com>
2024-09-30 16:09:50 +0530

    js random color generator

---
+++ app.js
@@ -0,0 +1,20 @@
+//random color genarator by js
+let btn = document.querySelector("button");
+
+btn.addEventListener("click", function(){
+console.log("genarate randomcolor");
+let h3 = document.querySelector("h3");
+ randomcolor = getRandomColor();
+ h3.innerText = randomcolor ;
+ let div = document.querySelector("div");
+ div.style.backgroundColor = randomcolor;
+
+});
+
+function getRandomColor (){
+    let red = Math.floor(Math.random() *255);
+    let green = Math.floor(Math.random()*255);
+    let blue = Math.floor(Math.random()*255);
+    let color = `rgb(${red},${green},${blue})`
+    return color;
+;}
